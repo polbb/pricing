d6e99e5d59aefd1bf214121f5b7dcf41d47d35b3
polbb <paulbessone@gmail.com>
2024-04-05 13:35:56 +0100

    sliders out

--- main.py
+++ main.py
@@ -50,22 +50,16 @@ with st.container(border=True):
     notice_pct_dist_x3 = col3.text_input("Right", value=".25")
     notice_pct_dist_x4 = col4.text_input("Size", value="100000")
 
-if data:
-    # st.write(number_of_simulations)
-    # st.write(f'Enter notice_pct_dist_x1: {notice_pct_dist_x1}')
-    # st.write(f'Enter notice_pct_dist_x2: {notice_pct_dist_x2}')
-    # st.write(f'Enter notice_pct_dist_x3: {notice_pct_dist_x3}')
-    # st.write(f'Enter notice_pct_dist_x4: {notice_pct_dist_x4}')
-
-    # Generate a distribution plot using the input values
-    fig = go.Figure(data=[go.Histogram(x=np.random.triangular(
-        float(notice_pct_dist_x1), 
-        float(notice_pct_dist_x2), 
-        float(notice_pct_dist_x3), 
-        int(notice_pct_dist_x4)
-    ))])
-    fig.update_layout(title_text='Notice Percentage Distribution Plot')
-    st.plotly_chart(fig)
+    if data:
+        # Generate a distribution plot using the input values
+        fig = go.Figure(data=[go.Histogram(x=np.random.triangular(
+            float(notice_pct_dist_x1), 
+            float(notice_pct_dist_x2), 
+            float(notice_pct_dist_x3), 
+            int(notice_pct_dist_x4)
+        ))])
+        fig.update_layout(title_text='Notice Percentage Distribution Plot', width=400, height=300)  # Adjusted size
+        st.plotly_chart(fig, use_container_width=True)  # Plot inside the container
 
     # for i in range(num_simulations):
 
