5a8ad2daf78133468a5cfc10ef770ca1201a51c6
polbb <paulbessone@gmail.com>
2024-04-05 11:31:58 +0100

    first commit

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import numpy as np
 # import seaborn as sns
 import random
 import numpy as np
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 import math
 from random import randrange
 from datetime import datetime
