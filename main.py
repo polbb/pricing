e45784650c04e83b6fcb56b937f5b05adabd57da
polbb <paulbessone@gmail.com>
2024-04-05 10:45:29 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,39 @@
+import datetime
+import boto3
+from bs4 import BeautifulSoup
+from fuzzywuzzy import fuzz
+import re
+import streamlit as st
+import pandas as pd
+from dotenv import load_dotenv
+import os
+import json
+
+# Streamlit UI
+st.set_page_config(layout="wide")  # Force wide mode
+with open("style.css") as css:
+    st.markdown(f'<style>{css.read()}</style>', unsafe_allow_html=True)
+
+# AWS Credentials
+aws_access_key_id = st.secrets.AWS_ACCESS_KEY_ID
+aws_secret_access_key = st.secrets.AWS_SECRET_ACCESS_KEY
+aws_default_region = st.secrets.AWS_DEFAULT_REGION
+
+# load_dotenv('./.env.txt')
+# aws_access_key_id = os.getenv('AWS_ACCESS_KEY_ID')
+# aws_secret_access_key = os.getenv('AWS_SECRET_ACCESS_KEY')
+# aws_default_region = os.getenv('AWS_DEFAULT_REGION')
+
+# AWS Services Clients
+dynamodb = boto3.resource('dynamodb', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key, region_name=aws_default_region)
+s3 = boto3.client('s3', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key, region_name=aws_default_region)
+
+st.title("ArgoXai")
+# col1, col2, _, _, _, _, _, _ = st.columns([3,3,1,1,1,1,1,1])
+# fuzzy_search = col1.text_input("Search Documents")
+data = st.button("Search")
+st.divider()
+
+if data:
+    st.write('test')
+
