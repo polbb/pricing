f348243dfe61681d0d20fc75d32fc63da54b4c5f
polbb <paulbessone@gmail.com>
2024-04-05 14:18:35 +0100

    3 distributions

--- utils.py
+++ utils.py
@@ -1,4 +1,10 @@
 import numpy as np
+import pandas as pd
+import random
+import math
+from random import randrange
+from datetime import datetime
+
 
 def severity_generator(notice_pct_dist, notice_pct_loss_dist, severity_dist):
     notice_pct = np.random.choice(notice_pct_dist)
@@ -9,3 +15,27 @@ def severity_generator(notice_pct_dist, notice_pct_loss_dist, severity_dist):
     high_severity_pct = (1 - low_severity_pct) * (1-med_high_severity_pct_generator)
     
     return notice_pct, notice_pct_loss, low_severity_pct, med_severity_pct, high_severity_pct
+
+def DV_generator(deal_count, DV_range, sme_low_DV, sme_upper_DV, mm_low_DV, mm_upper_DV, sme_pct, mm_pct, j_pct, j_low_DV, j_upper_DV):
+    DV_list = []
+    sme_count = int(deal_count*sme_pct)
+    mm_count = int(deal_count*mm_pct)
+    j_count = int(deal_count*j_pct)
+    for x in range(sme_count):
+        DV_list.append(random.randrange(sme_low_DV, sme_upper_DV, DV_range)) 
+    for x in range(mm_count):
+        DV_list.append(random.randrange(mm_low_DV, mm_upper_DV, DV_range)) 
+    for x in range(j_count):
+        DV_list.append(random.randrange(j_low_DV, j_upper_DV, DV_range))  
+    if (sme_count + mm_count + j_count) != deal_count:
+        for i in range(deal_count - len(DV_list)):
+            x = random.randrange(1, 4)
+            if x == 1:
+                DV_list.append(random.randrange(sme_low_DV, sme_upper_DV, DV_range)) 
+            if x == 2:
+                DV_list.append(random.randrange(mm_low_DV, mm_upper_DV, DV_range))
+            if x == 3:
+                DV_list.append(random.randrange(j_low_DV, j_upper_DV, DV_range))
+    random.shuffle(DV_list)
+    
+    return DV_list
