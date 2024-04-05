ece78d0556ffb8867e6487aa00fa09cdc205962b
polbb <paulbessone@gmail.com>
2024-04-05 11:35:54 +0100

    first commit

--- main.py
+++ main.py
@@ -44,10 +44,10 @@ col1, col2, col3, col4, _, _, _, _ = st.columns([2,2,2,2,1,1,1,1])
 notice_pct_dist_x1 = col1.text_input("Enter notice_pct_dist_x1", value=".05")
 notice_pct_dist_x2 = col2.text_input("Enter notice_pct_dist_x2", value=".15")
 notice_pct_dist_x3 = col3.text_input("Enter notice_pct_dist_x3", value=".25")
-notice_pct_dist_x4 = col4.text_input("Enter notice_pct_dist_x4", value="100_000")
+notice_pct_dist_x4 = col4.text_input("Enter notice_pct_dist_x4", value="100000")
 
 
-
+s
 if data:
 
     st.write(number_of_simulations)
